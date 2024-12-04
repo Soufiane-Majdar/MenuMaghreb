1ac1b33e2ae547caaafccb5ae2f200259f5323ce
Soufiane Majdar <soufiane.majdar@gmail.com>
2024-12-05 00:03:05 +0100

    new update be hosted on vercel vf=6

--- qr_menu_core/settings.py
+++ qr_menu_core/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static") 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Keep original static files here
 ]
