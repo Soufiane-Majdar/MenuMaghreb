cf3b616871ae56a34f41803e02b7db7ca253ca5a
Soufiane Majdar <soufiane.majdar@gmail.com>
2024-12-04 23:32:43 +0100

    new update be hosted on vercel vf=2 vercel config changed

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+
+# Install dependencies
 pip3.12 install -r requirements.txt
 
-# make migrations
+# Make migrations
 python3.12 manage.py migrate 
-# python3.12 manage.py collectstatic --noinput
+
+# Create static files directory
+mkdir -p staticfiles_build
+
+# Collect static files
+python3.12 manage.py collectstatic --noinput --clear
+
+# Copy admin static files
+cp -r /usr/local/lib/python3.12/site-packages/django/contrib/admin/static/admin staticfiles_build/static/
+
+# Copy Jazzmin static files
+cp -r /usr/local/lib/python3.12/site-packages/jazzmin/static/jazzmin staticfiles_build/static/
+
+# Set permissions
+chmod -R 755 staticfiles_build/
